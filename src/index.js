ac288376376796bd6a21f51a15ae7c8d4c2cbff0
Kadirgu <Kadirgu@users.noreply.github.com>
2022-07-20 15:59:40 +0200

    update index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './service-worker';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
